bb334cf71ef40d71fc899bae2db59d943006ac11
Sooraj Gupta <soorajgupta00@gmail.com>
2024-01-13 15:19:02 +0530

    fixed error

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const Navbar = () => {
   return (
     <>
       <nav className="fixed my-0 top-0 w-full text-center py-2 z-50  bg-gray-200 shadow-lg bg-blend-multiply ">
-        This is made by AV <i> - ©️</i>{' '}
+        This is made by AV <i> - ©️</i>{" "}
         <a href="https://github.com/Ingenious0/">GITHUB</a>
       </nav>
     </>
